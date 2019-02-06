9bd8fee930236e9c6aea07c6f727a69f432c18cd
Umidbek Karimov <uma.karimov@gmail.com>
2019-02-07 02:13:09 +0500

    fix(useForm): Sync options with form. (#20)

---
+++ src/internal/shallowEqual.js
@@ -0,0 +1,23 @@
+const shallowEqual = (a, b) => {
+  if (a === b) {
+    return true
+  }
+  if (typeof a !== 'object' || !a || typeof b !== 'object' || !b) {
+    return false
+  }
+  const keysA = Object.keys(a)
+  const keysB = Object.keys(b)
+  if (keysA.length !== keysB.length) {
+    return false
+  }
+  const bHasOwnProperty = Object.prototype.hasOwnProperty.bind(b)
+  for (let idx = 0; idx < keysA.length; idx++) {
+    const key = keysA[idx]
+    if (!bHasOwnProperty(key) || a[key] !== b[key]) {
+      return false
+    }
+  }
+  return true
+}
+
+export default shallowEqual
